88bf268ff8d6899fb577714fad405c6589676ed4
CWC-88 <29718763+CWC-88@users.noreply.github.com>
2020-05-18 20:16:01 -0500

    DONE

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,11 +3,32 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import axiosWithAuth from "../axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
+const [update, setUpdate] = useState(false);
+
+// fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+const fetchData = () =>{
+axiosWithAuth()
+.get(`http://localhost:5000/api/colors`)
+.then((res) =>{
+  console.log("cuhhla", res.data);
+setColorList(res.data)
+})
+.catch((err)=>{
+  console.log(err)
+});
+
+};
+
+useEffect(() =>{
+  fetchData();
+  setUpdate(false);
+},[update]);
+
 
   return (
     <>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import {axiosWithAuth} from '../axiosWithAuth'
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+const [credentials, setCredentials] = useState({});
+const login = e => {
+  e.preventDefault();
+  axiosWithAuth().post("http://localhost:5000/api/login", credentials)
+  .then(res => {
+      localStorage.setItem('token', res.data.payload);
+      props.history.push('/bubbles');
+  })
+}
 
-export default Login;
+const handleChange = e =>{
+  setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+  })
+}
+return(
+  <div>
+      <form onSubmit={login}>
+          <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}/>
+<input
+  type="text"
+  name="password"
+  value={credentials.password}
+  onChange={handleChange}/>
+
+  <button>Log in</button>
+
+      </form>
+  </div>
+)
+
+
+}
+
+
+export default Login
